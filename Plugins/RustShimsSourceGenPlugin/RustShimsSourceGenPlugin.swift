7ad6d9a944416c05e13cadb76755a4d904497370
egdeeno <users.noreply.github.com>
2023-10-04 00:20:54 +0400

    added os arg to RustShims/build.sh

--- Plugins/RustShimsSourceGenPlugin/RustShimsSourceGenPlugin.swift
+++ Plugins/RustShimsSourceGenPlugin/RustShimsSourceGenPlugin.swift
@@ -9,15 +9,26 @@ struct SwiftGenPlugin: BuildToolPlugin {
         //let output = target.directory.appending("GeneratedEnum.swift")//context.pluginWorkDirectory.appending("GeneratedEnum.swift")
         var utsnameInstance = utsname()
         uname(&utsnameInstance)
-        let cpuArch: String = withUnsafePointer(to: &utsnameInstance.machine) {
+        var cpuArch: String = withUnsafePointer(to: &utsnameInstance.machine) {
              $0.withMemoryRebound(to: CChar.self, capacity: 1) {
                 ptr in String.init(validatingUTF8: ptr)
              }
         } ?? ""
+        if cpuArch == "arm64" {
+          cpuArch = "aarch64"
+        }
+        var os = "apple-darwin"
+        #if os(iOS)
+           os = "apple-ios"
+        #elseif os(Linux)
+           os = "unknown-linux-gnu"
+        #elseif os(Windows)
+           os = "pc-windows-msvc"
+        #endif
         return [
             .prebuildCommand(displayName: "Generate static Lib",
                           executable: target.directory.appending("build.sh"),//Path("/usr/bin/make"),//.init("/usr/bin/make"), //try context.tool(named: "/usr/bin/make").path,
-                          arguments: [target.directory, cpuArch],
+                          arguments: [target.directory, cpuArch, os],
                           environment: [:],
                           outputFilesDirectory: target.directory)
         ]
--- Sources/RustShims/build.sh
+++ Sources/RustShims/build.sh
@@ -5,6 +5,9 @@ pwd
 echo "cpu arch:"
 echo $2
 
+echo "os:"
+echo $3
+
 export PATH=$PATH:/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin
 
 export CARGO_HOME=/mnt/ext/etc/.cargo
@@ -17,8 +20,8 @@ export RUST_SRC_PATH=/mnt/ext/etc/projects/rust_projects/rust/src
 PATH=$PATH:$RUST_SRC_PATH
 
 rustup override set nightly
-rustup target add $2-unknown-linux-gnu
+rustup target add $2-$3
 
-cargo -Z unstable-options -C $1 build --release
+cargo -Z unstable-options -C $1 build --release --target $2-$3
 
-cp $1/target/$2-unknown-linux-gnu/release/*.a $1/
+cp $1/target/$2-$3/release/*.a $1/
